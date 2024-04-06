43a0a4ed86621d5fa4dac4d3e3994d81e081fa77
bielelestudiante <biel.serrallonga@estudiantat.upc.edu>
2024-04-06 17:53:49 +0200

    health bar

---
+++ Assets/Testing Zone/Scripts/Player_health.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_health : MonoBehaviour
+{
+    public float MaxHealth = 100f;
+    public float CurrentHealth = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
