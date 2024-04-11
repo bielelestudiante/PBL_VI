1ccbea5aa80af7b51bd7e0f5840e7793107da09b
Solan de Cabras, Cuenca <unai.laguarta@estudiantat.upc.edu>
2024-04-11 09:47:43 +0200

    Pantalla de Carga (Update 2.1 Carlos putero)
    Ajuste del botón exit
    Configuración de las escenas para cada botón usando una string personalizable

--- Assets/UI UX/Start Game Screen/MenuPrincipal.cs
+++ Assets/UI UX/Start Game Screen/MenuPrincipal.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    public void Start()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+    // Lista de nombres de las escenas
+    public string[] sceneNames;
 
-    public void Salir()
+    // Método que se llama cuando se hace click
+    public void LoadSceneOnClick(int sceneIndex)
     {
-        Debug.Log("Salir...");
-        Application.Quit();   
+        // Verificar si el índice está dentro del rango válido
+        if (sceneIndex >= 0 && sceneIndex < sceneNames.Length)
+        {
+            // Obtener el nombre de la escena usando el índice
+            string sceneToLoad = sceneNames[sceneIndex];
+
+            // Cargar la escena por nombre
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogError("Índice de escena fuera de rango.");
+        }
     }
 }
