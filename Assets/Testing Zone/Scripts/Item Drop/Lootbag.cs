9929989f2d9691ef37b08f4dde7dd05c35ca55a5
Solan de Cabras, Cuenca <unai.laguarta@estudiantat.upc.edu>
2024-04-15 09:51:27 +0200

    Loot Drop (update 1.0)
    Configuración de los dropeos de los items de los enemigos de los juegos de los delox detox
    
    https://www.youtube.com/watch?v=KjvvRmG7PBM
    
    min 7:37

---
+++ Assets/Testing Zone/Scripts/Item Drop/Lootbag.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lootbag : MonoBehaviour
+{
+    public GameObject droppedItemPrefab;
+    public List<Loot> lootList = new List<Loot>();
+    
+    Loot GetDroppedItem()
+    {
+        int randomNumber = Random.Range(1, 101); // 1 - 100
+        List<Loot> possibleItems = new List<Loot>();
+
+        foreach (Loot item in lootList)
+        {
+            if (randomNumber <= item.dropChance)
+            {
+                possibleItems.Add(item);
+            }
+        }
+    }
+}
