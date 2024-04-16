4b849e94a4d82e7be713e86cbdf7a0a8a5d90cfd
bielelestudiante <biel.serrallonga@estudiantat.upc.edu>
2024-04-16 08:53:43 +0200

    player health system

---
+++ Assets/Testing Zone/Scripts/PlayerHealthSystem.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthSystem : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    public Color originalColor;
+    public Color damageColor;
+    public float damageFlashTime = 0.2f;
+
+    private Material myMaterial;
+    private float flashTimer;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        myMaterial = GetComponent<Renderer>().material;
+        originalColor = myMaterial.color;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PlayerTakesDamage(25);
+        }
+        if(flashTimer > 0)
+        {
+            flashTimer -= Time.deltaTime;
+            myMaterial.color = damageColor;
+        }
+        else
+        {
+            myMaterial.color = originalColor;
+        }
+    }
+
+    private void PlayerTakesDamage(int damage)
+    {
+        currentHealth -= damage;
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+        flashTimer = damageFlashTime;
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
