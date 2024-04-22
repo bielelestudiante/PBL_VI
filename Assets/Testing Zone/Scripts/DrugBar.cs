9ca7e8bfd0e38636c2d28faa91f4b53aebb983ea
bielelestudiante <biel.serrallonga@estudiantat.upc.edu>
2024-04-22 12:06:18 +0200

    drug bar decay

--- Assets/Testing Zone/Scripts/DrugBar.cs
+++ Assets/Testing Zone/Scripts/DrugBar.cs
@@ -6,16 +6,18 @@ using UnityEngine.UI;
 public class DrugBar : MonoBehaviour
 {
     public Image barraDeDrogaDer;
-
     public Image barraDeDrogaIzq;
 
     public float drogaActual;
-
     public float drogaMaxima;
+
+    public float decayRate = 1.5f;
 
 
     private void Update()
     {
+        drogaActual = Mathf.Clamp(drogaActual - decayRate * Time.deltaTime, 0f, drogaMaxima);
+
         barraDeDrogaDer.fillAmount = drogaActual / drogaMaxima;
         barraDeDrogaIzq.fillAmount = drogaActual / drogaMaxima;
     }
