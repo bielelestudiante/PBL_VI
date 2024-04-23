9abc5e58c8cde01db15b3d166399c21f2ce55940
ManolitoPiruleta58 <carlos.peinado@estudiantat.upc.edu>
2024-04-23 10:58:58 +0200

    pusheao

---
+++ Assets/Assets/Personaje/Animaciones/UpperbodyAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpperbodyAnimation : MonoBehaviour
+{
+    Animator _animator;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
